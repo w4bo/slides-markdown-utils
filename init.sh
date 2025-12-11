677ea16be0d41c6f12c1cf961eb3d19f1e6e9253
w4bo <m.francia@unibo.it>
2025-12-11 09:36:23 +0100

    copy the dockerfile as well

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ set -xo
 [ ! -f publish.sh ] && cp utils/publish.sh.example publish.sh
 # ln -s utils/_quarto.yml _quarto.yml
 [ ! -f _quarto.yml ] && cp utils/_quarto.yml _quarto.yml
-ln -s utils/docker-compose.yml docker-compose.yml
+[ ! -f docker-compose.yml ] && cp utils/docker-compose.yml docker-compose.yml
